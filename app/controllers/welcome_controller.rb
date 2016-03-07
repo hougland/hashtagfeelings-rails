98ad13373274ed3c2087b4de15a7f771c61523b9
Ricky Hougland <houglande@gmail.com>
2016-03-07 11:49:49 -0800

    add gems, add basic view page for show

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,16 @@
 class WelcomeController < ApplicationController
   def index
   end
+
+  def show
+    if params[:feeling] == "good"
+      @feeling = "good"
+    elsif params[:feeling] == "bad"
+      @feeling = "bad"
+    end
+    # make get request for API call, either good or bad
+    # (optional tech would be to create a "wrapper" or gem for my API)
+    # if call successful: parse necessary info from API call
+    # if not successful, flash error message and render :index
+  end
 end
