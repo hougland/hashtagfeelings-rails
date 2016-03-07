138501f8bf5a8635219b972081bde6c5e89662f6
Ricky Hougland <houglande@gmail.com>
2016-03-07 12:01:33 -0800

    successfully calls back end api

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,14 +3,12 @@ class WelcomeController < ApplicationController
   end
 
   def show
-    if params[:feeling] == "good"
-      @feeling = "good"
-    elsif params[:feeling] == "bad"
-      @feeling = "bad"
+    @hashtag = HTTParty.get("https://hashtagfeelings.herokuapp.com/#{params[:feeling]}")
+    if response.code == 500
+      flash[:error] = "Button currently not working."
+      render :index
+    else
+      render :show
     end
-    # make get request for API call, either good or bad
-    # (optional tech would be to create a "wrapper" or gem for my API)
-    # if call successful: parse necessary info from API call
-    # if not successful, flash error message and render :index
   end
 end
