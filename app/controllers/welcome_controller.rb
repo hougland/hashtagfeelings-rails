0d3f6547c5486eaa753d4c1628558ddd381f32e5
Ricky Hougland <houglande@gmail.com>
2016-03-10 14:44:25 -0800

    add twitter gem and play around with embedding tweets

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,6 +4,8 @@ class WelcomeController < ApplicationController
 
   def show
     @hashtag = HTTParty.get("https://hashtagfeelings.herokuapp.com/#{params[:feeling]}")
+    @tweets = $twitter.search("to:justinbieber marry me", result_type: "recent").take(3)
+
     if response.code == 500
       flash[:error] = "Button currently not working."
       render :index
