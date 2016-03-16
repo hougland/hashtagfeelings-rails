53892358edffe94c6362bed7c3cf2d94cd1d3bbb
Ricky Hougland <houglande@gmail.com>
2016-03-16 11:17:53 -0700

    add link to about page in navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'welcome#index'
   post '/' => 'welcome#show'
-  get '/about' => 'welcome#about'
+  get '/about' => 'welcome#about', as: :about
 
   get "/auth/:provider/callback", to: "sessions#create"
   delete "/logout", to: 'sessions#destroy', as: :logout
